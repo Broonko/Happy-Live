5222e65b51af1715cfe820a0bfd0d9e91a50b349
broonko <broonko@hotmail.com>
2021-02-05 01:50:48 +0000

    V1.0 heroku

--- public/js/artists.js
+++ public/js/artists.js
@@ -4,17 +4,16 @@ api
         const artists = document.getElementById('artistsPhotos')
         response.data.forEach(artist => {
             if (artist.artist.genre) {
-            newArtist = document.createElement('a')
-            newArtist.setAttribute('href', 'profilePublic.html')
-            newArtist.innerHTML = `
+                newArtist = document.createElement('a')
+                newArtist.setAttribute('href', 'profilePublic.html')
+                newArtist.innerHTML = `
             <img src="${artist.photo}" title="${artist.name}" class="img-fluid rounded p-3 d-inline-block desvanecer" alt="...">
             `
-            artists.appendChild(newArtist)
-            }
-            newArtist.addEventListener('click', () => {
-                localStorage.setItem('artistId', artist._id) 
-            })
+                artists.appendChild(newArtist)
 
-        })
+                newArtist.addEventListener('click', () => {
+                    localStorage.setItem('artistId', artist._id)
+                })
+            })
     })
     .catch(err => { alert('do not have permission') })
